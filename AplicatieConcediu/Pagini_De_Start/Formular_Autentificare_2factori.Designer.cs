9391b58bcd08cc3a38405f48398f790e8c1bbe6e
adelinazmarandoiu <adelinazmarandoiu@totalsoft.ro>
2022-08-30 15:37:00 +0300

    v2 cod verificare

--- AplicatieConcediu/Pagini_De_Start/Formular_Autentificare_2factori.Designer.cs
+++ AplicatieConcediu/Pagini_De_Start/Formular_Autentificare_2factori.Designer.cs
@@ -31,41 +31,56 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(97, 140);
+            this.label1.Location = new System.Drawing.Point(129, 172);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(218, 13);
+            this.label1.Size = new System.Drawing.Size(273, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Introduceti codul de verificare trimis pe email:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(377, 137);
+            this.textBox1.Location = new System.Drawing.Point(503, 169);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(240, 20);
+            this.textBox1.Size = new System.Drawing.Size(319, 22);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(289, 265);
+            this.button1.Location = new System.Drawing.Point(200, 346);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 33);
+            this.button1.Size = new System.Drawing.Size(169, 41);
             this.button1.TabIndex = 2;
             this.button1.Text = "Validare cod";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(639, 346);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Retrimitere Cod";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Formular_Autentificare_2factori
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Formular_Autentificare_2factori";
             this.Text = "Formular_Autentificare_2factori";
             this.ResumeLayout(false);
@@ -78,5 +93,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
