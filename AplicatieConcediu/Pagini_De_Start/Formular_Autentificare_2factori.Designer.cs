7ab8fe171b6981dbb768da9dd3693b060a290529
adelinazmarandoiu <adelinazmarandoiu@totalsoft.ro>
2022-08-30 15:33:47 +0300

    s

--- AplicatieConcediu/Pagini_De_Start/Formular_Autentificare_2factori.Designer.cs
+++ AplicatieConcediu/Pagini_De_Start/Formular_Autentificare_2factori.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Formular_Autentificare_2factori
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Formular_Autentificare_2factori";
             this.Text = "Formular_Autentificare_2factori";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
