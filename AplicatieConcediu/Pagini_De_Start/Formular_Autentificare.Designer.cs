4890e46a241760c5cccec26ced210abfa9d5ee9a
lieautman <dumi_cristi@yahoo.com>
2022-08-30 15:55:41 +0300

    repair2

--- AplicatieConcediu/Pagini_De_Start/Formular_Autentificare.Designer.cs
+++ AplicatieConcediu/Pagini_De_Start/Formular_Autentificare.Designer.cs
@@ -130,7 +130,6 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
