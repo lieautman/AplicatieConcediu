1b72ca504160674b27b34b1610b092ff5016d5f9
Andra Iancu <95624161+xAndruti@users.noreply.github.com>
2022-08-30 15:58:08 +0300

    1st

--- AplicatieConcediu/Pagini_Concedii/Pagina_ConcediileMele.Designer.cs
+++ AplicatieConcediu/Pagini_Concedii/Pagina_ConcediileMele.Designer.cs
@@ -28,34 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.Label label1;
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pagina_ConcediileMele));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(121, 23);
+            this.dataGridView1.Location = new System.Drawing.Point(28, 174);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(565, 386);
+            this.dataGridView1.Size = new System.Drawing.Size(597, 203);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(266, 9);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(81, 13);
+            label1.TabIndex = 1;
+            label1.Text = "Concediile mele";
+            label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Pagina_ConcediileMele
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(658, 398);
+            this.Controls.Add(label1);
             this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Pagina_ConcediileMele";
             this.Text = "ConcediileMele";
             this.Load += new System.EventHandler(this.Pagina_ConcediileMele_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
--- AplicatieConcediu/Pagini_Concedii/Pagina_ConcediileMele.cs
+++ AplicatieConcediu/Pagini_Concedii/Pagina_ConcediileMele.cs
@@ -49,5 +49,15 @@ namespace AplicatieConcediu
 
             conn.Close();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
