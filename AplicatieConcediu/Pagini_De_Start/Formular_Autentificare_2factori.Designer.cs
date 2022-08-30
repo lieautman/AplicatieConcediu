e2077553aa76d9aab6235e60ce48c11b6c5f9846
elena.craciun <elena.craciun@TS1542>
2022-08-30 16:36:50 +0300

    t

--- AplicatieConcediu/Pagini_De_Start/Formular_Autentificare_2factori.Designer.cs
+++ AplicatieConcediu/Pagini_De_Start/Formular_Autentificare_2factori.Designer.cs
@@ -51,7 +51,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(289, 265);
+            this.button1.Location = new System.Drawing.Point(264, 256);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(127, 33);
             this.button1.TabIndex = 2;
