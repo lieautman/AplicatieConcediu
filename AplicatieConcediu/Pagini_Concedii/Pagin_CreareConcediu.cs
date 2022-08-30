7acc397021a1e32a58a2bbf5be9bd40b10932b91
Teodor Ungureanu <teodor.ungureanu@totalsoft.ro>
2022-08-30 15:28:33 +0300

    creare concediu

---
+++ AplicatieConcediu/Pagini_Concedii/Pagin_CreareConcediu.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplicatieConcediu.Pagini_Concedii
+{
+    public partial class Pagin_CreareConcediu : Form
+    {
+        public Pagin_CreareConcediu()
+        {
+            InitializeComponent();
+        }
+
+        private void Pagin_CreareConcediu_Load(object sender, EventArgs e)
+        {
+            DataSet dt = new DataSet();
+            try
+            {
+                //sql connection object
+                using (SqlConnection conn = new SqlConnection(Globals.ConnString))
+                {
+
+                    //retrieve the SQL Server instance version
+                    string query = string.Format(" SELECT * FROM TipConcediu");
+                    //define the SqlCommand object
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    //open connection
+                    conn.Open();
+
+                    //execute the SQLCommand
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    Console.WriteLine(Environment.NewLine + "Retrieving data from database..." + Environment.NewLine);
+                    Console.WriteLine("Retrieved records:");
+
+                    //check if there are records
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            var x = dr.GetValue(1);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No data found.");
+                    }
+
+                    //close data reader
+                    dr.Close();
+
+                    //close connection
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                //display error message
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+        }
+
+
+    }
+}
