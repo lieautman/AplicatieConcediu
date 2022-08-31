67dfe85f596446e0215defcafab2c0aaa6a2f659
lieautman <dumi_cristi@yahoo.com>
2022-08-31 15:14:05 +0300

    repaired a form(removed bar)

--- AplicatieConcediu/Pagini_Profil/PaginaCuTotateEchipele.Designer.cs
+++ AplicatieConcediu/Pagini_Profil/PaginaCuTotateEchipele.Designer.cs
@@ -47,7 +47,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(13, 31);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(161, 55);
             this.button1.TabIndex = 4;
@@ -134,7 +134,8 @@
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "PaginaCuTotateEchipele";
             this.Text = "PaginaCuTotiAngajatii";
             this.Load += new System.EventHandler(this.PaginaCuTotiAngajatii_Load);
