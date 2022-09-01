138de18c03163d5a83ad19324a5a5757d72ef4b0
adelinazmarandoiu <adelinazmarandoiu@totalsoft.ro>
2022-09-01 16:40:52 +0300

    nimic

--- AplicatieConcediu/Pagini_Profil/PaginaCuTotateEchipele.cs
+++ AplicatieConcediu/Pagini_Profil/PaginaCuTotateEchipele.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace AplicatieConcediu.Pagini_Profil
 {
+     
     public partial class PaginaCuTotateEchipele : Form
     {
         public PaginaCuTotateEchipele()
         {
             InitializeComponent();
         }
-
-
+        public List<byte[]> PozaLista = new List<byte[]>();
+        
 
         //trebuie create automat din cod si de acolo trebuie incarcate pozele si tot
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -82,6 +85,26 @@ namespace AplicatieConcediu.Pagini_Profil
 
         private void PaginaCuTotateEchipele_Load(object sender, EventArgs e)
         {
+            byte[] poza = { };
+            bool isOk = true;
+            string query1 = "SELECT Poza FROM Echipa ";
+            SqlConnection connection1 = new SqlConnection();
+            SqlDataReader reader1 = Globals.executeQuery(query1, out connection1);
+
+            while (reader1.Read())
+            {
+                if (reader1["Poza"] != DBNull.Value)
+                    poza = (byte[])reader1["Poza"];
+                 
+                else
+                    isOk = false;
+
+            }
+            reader1.Close();
+            connection1.Close();
+            if (isOk == true)
+                pictureBox2.Image = System.Drawing.Image.FromStream(new MemoryStream(poza));
+            
 
         }
     }
