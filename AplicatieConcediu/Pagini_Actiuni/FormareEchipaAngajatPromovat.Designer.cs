12179b80d8ef0c2cb195e417aa7bd09b01ff7b26
Andra Iancu <95624161+xAndruti@users.noreply.github.com>
2022-09-01 15:08:50 +0300

    design pag formare echipa

--- AplicatieConcediu/Pagini_Actiuni/FormareEchipaAngajatPromovat.Designer.cs
+++ AplicatieConcediu/Pagini_Actiuni/FormareEchipaAngajatPromovat.Designer.cs
@@ -35,8 +35,8 @@
             this.label3 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -54,26 +54,35 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
+            this.label1.Image = global::AplicatieConcediu.Properties.Resources.BackGround;
             this.label1.Location = new System.Drawing.Point(12, 152);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(178, 13);
+            this.label1.Size = new System.Drawing.Size(249, 15);
             this.label1.TabIndex = 1;
             this.label1.Text = "Lista angajati pentru formare echipa:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
+            this.label2.Image = global::AplicatieConcediu.Properties.Resources.BackGround;
             this.label2.Location = new System.Drawing.Point(12, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.Size = new System.Drawing.Size(97, 15);
             this.label2.TabIndex = 2;
             this.label2.Text = "Manager nou:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(682, 142);
+            this.button1.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
+            this.button1.Image = global::AplicatieConcediu.Properties.Resources.BackGround;
+            this.button1.Location = new System.Drawing.Point(665, 120);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 23);
+            this.button1.Size = new System.Drawing.Size(123, 45);
             this.button1.TabIndex = 3;
             this.button1.Text = "Adauga in echipa";
             this.button1.UseVisualStyleBackColor = true;
@@ -82,16 +91,20 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(87, 108);
+            this.label3.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
+            this.label3.Image = global::AplicatieConcediu.Properties.Resources.BackGround;
+            this.label3.Location = new System.Drawing.Point(105, 111);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.Size = new System.Drawing.Size(121, 15);
             this.label3.TabIndex = 4;
             this.label3.Text = "NUME PRENUME";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(90, 9);
+            this.pictureBox1.Location = new System.Drawing.Point(108, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(100, 96);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -102,38 +115,49 @@
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
+            this.label5.Image = global::AplicatieConcediu.Properties.Resources.BackGround;
             this.label5.Location = new System.Drawing.Point(256, 13);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.Size = new System.Drawing.Size(52, 15);
             this.label5.TabIndex = 8;
             this.label5.Text = "Echipa";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(713, 13);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "back";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(302, 12);
+            this.comboBox1.Location = new System.Drawing.Point(314, 12);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 10;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Rockwell", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(183)))), ((int)(((byte)(164)))));
+            this.button2.Image = global::AplicatieConcediu.Properties.Resources.BackGround;
+            this.button2.Location = new System.Drawing.Point(737, 9);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(52, 60);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "⮌";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
             // FormareEchipaAngajatPromovat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::AplicatieConcediu.Properties.Resources.BackGround;
             this.ClientSize = new System.Drawing.Size(800, 441);
-            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label3);
@@ -161,7 +185,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- AplicatieConcediu/Pagini_Actiuni/FormareEchipaAngajatPromovat.cs
+++ AplicatieConcediu/Pagini_Actiuni/FormareEchipaAngajatPromovat.cs
@@ -157,121 +157,27 @@ namespace AplicatieConcediu.Pagini_Actiuni
             this.Close();
             f.ShowDialog();
 
-            //SqlConnection conn1 = new SqlConnection();
-            //SqlDataReader reader1 = Globals.executeQuery("Select Nume, Prenume from Angajat where Email = '" + Globals.EmailManager + "'", out conn1);
-            //string numesiprenume = "";
-            //while (reader1.Read())
-            //{
-            //    numesiprenume += reader1["Nume"];
-            //    numesiprenume += " ";
-            //    numesiprenume += reader1["Prenume"];
-            //}
-            //reader1.Close();
-            //conn1.Close();
-            //label3.Text = numesiprenume;
-
-
-            //SqlConnection conn = new SqlConnection();
-            //SqlDataReader reader = Globals.executeQuery("Select Nume, Prenume, Email,DataAngajarii, DataNasterii, CNP, idEchipa from Angajat where ManagerId is not null ", out conn);
-            //while (reader.Read())
-            //{
-            //    string nume = (string)reader["Nume"];
-            //    string prenume = (string)reader["Prenume"];
-            //    string email = (string)reader["Email"];
-            //    DateTime dataAngajarii;
-            //    if (reader[4] != DBNull.Value)
-            //        dataAngajarii = (DateTime)reader[4];
-            //    else dataAngajarii = (DateTime)reader[""];
-
-
-            //    DateTime dataNasterii = (DateTime)reader["DataNasterii"];
-            //    string cNP = (string)reader["CNP"];
-            //    int idEchipaa;
-            //    if (reader[6] != DBNull.Value)
-            //    {
-            //        idEchipaa = Convert.ToInt32(reader.GetValue(6));
-            //    }
-            //    else idEchipaa = -1;
-
-            //    AngajatiListaPentruFormareEchipaNoua angajat = new AngajatiListaPentruFormareEchipaNoua(nume, prenume, email, dataAngajarii, dataNasterii, cNP, idEchipaa);
-            //    listaAngajati.Add(angajat);
-            //}
-
-            //dataGridView1.DataSource = listaAngajati;
-
-            //conn.Close();
-
-            //byte[] poza = { };
-            //bool isOk = true;
-            //string query2 = "SELECT Poza FROM Angajat WHERE Email ='" + Globals.EmailManager + "'";
-            //SqlConnection connection2 = new SqlConnection();
-            //SqlDataReader reader2 = Globals.executeQuery(query2, out connection2);
-
-            //while (reader2.Read())
-            //{
-            //    if (reader2["Poza"] != DBNull.Value)
-            //        poza = (byte[])reader2["Poza"];
-            //    else
-            //        isOk = false;
-
-            //}
-            //reader2.Close();
-            //connection2.Close();
-            //if (isOk == true)
-            //    pictureBox1.Image = System.Drawing.Image.FromStream(new MemoryStream(poza));
-
-
-
-            ////combobox
-            //using (SqlConnection connEchipa = new SqlConnection(Globals.ConnString))
-            //{
-            //    string query = string.Format("select Nume from Echipa");
-
-            //    //definire comenzi
-            //    SqlCommand cmd = new SqlCommand(query, connEchipa);
-            //    connEchipa.Open();
-            //    SqlDataReader dr = cmd.ExecuteReader();
-            //    Console.WriteLine(Environment.NewLine + "Retrieving data from database..." + Environment.NewLine);
-            //    Console.WriteLine("Retrieved records:");
-
-            //    //verificare existenta inregistrari
-            //    if (dr.HasRows)
-            //    {
-            //        while (dr.Read())
-            //        {
-            //            var echipa = new Echipa();
-            //            var y = dr.GetValue(0);
-            //            echipa.Nume = y.ToString();
-            //            numeEchipa.Add(echipa);
-            //        }
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("No data found.");
-            //    }
-            //    dr.Close();
-
-            //    connEchipa.Close();
-            //    comboBox1.DataSource = numeEchipa;
-            //    comboBox1.DisplayMember = "Nume";
-            //    comboBox1.ValueMember = "Id";
-            //}
-
-            //dataGridView1.Refresh();
-
-
-
-
-
-
-
-
 
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
