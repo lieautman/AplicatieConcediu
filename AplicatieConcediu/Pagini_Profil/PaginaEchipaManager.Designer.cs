376acc8e7f1b6662ee63855c0c13c0a0e385b652
Andra Iancu <95624161+xAndruti@users.noreply.github.com>
2022-09-02 15:07:12 +0300

    pagina vizualizare echipa manager

--- AplicatieConcediu/Pagini_Profil/PaginaEchipaManager.Designer.cs
+++ AplicatieConcediu/Pagini_Profil/PaginaEchipaManager.Designer.cs
@@ -33,18 +33,20 @@
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button2
             // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Rockwell", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(183)))), ((int)(((byte)(164)))));
             this.button2.Image = global::AplicatieConcediu.Properties.Resources.BackGround;
-            this.button2.Location = new System.Drawing.Point(737, 11);
+            this.button2.Location = new System.Drawing.Point(578, 11);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(52, 60);
@@ -55,10 +57,14 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 189);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 260);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(776, 249);
+            this.dataGridView1.Size = new System.Drawing.Size(617, 99);
             this.dataGridView1.TabIndex = 13;
             // 
             // label1
@@ -77,18 +83,19 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(12, 38);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 99);
+            this.pictureBox1.Size = new System.Drawing.Size(179, 176);
             this.pictureBox1.TabIndex = 15;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold);
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(183)))), ((int)(((byte)(164)))));
             this.button1.Image = global::AplicatieConcediu.Properties.Resources.BackGround;
-            this.button1.Location = new System.Drawing.Point(615, 134);
+            this.button1.Location = new System.Drawing.Point(456, 196);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(173, 49);
             this.button1.TabIndex = 16;
@@ -96,12 +103,26 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
+            this.label2.Image = global::AplicatieConcediu.Properties.Resources.BackGround;
+            this.label2.Location = new System.Drawing.Point(257, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 20);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Echipa mea";
+            // 
             // PaginaEchipaManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::AplicatieConcediu.Properties.Resources.BackGround;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(641, 371);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
@@ -110,6 +131,8 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "PaginaEchipaManager";
             this.Text = "PaginaEchipaManager";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.PaginaEchipaManager_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -124,5 +147,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- AplicatieConcediu/Pagini_Profil/PaginaEchipaManager.cs
+++ AplicatieConcediu/Pagini_Profil/PaginaEchipaManager.cs
@@ -1,4 +1,5 @@
-﻿using AplicatieConcediu.Pagini_Actiuni;
+﻿using AplicatieConcediu.DB_Classess;
+using AplicatieConcediu.Pagini_Actiuni;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,11 +9,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using AplicatieConcediu;
+using AplicatieConcediu.DB_Classess;
+using System.IO;
 
 namespace AplicatieConcediu.Pagini_Profil
 {
     public partial class PaginaEchipaManager : Form
     {
+        private List<VizualizareEchipaManager> lista=new List<VizualizareEchipaManager>();
         public PaginaEchipaManager()
         {
             InitializeComponent();
@@ -29,6 +35,64 @@ namespace AplicatieConcediu.Pagini_Profil
             this.Hide();
             membruNou.ShowDialog();
             this.Show();
+        }
+
+        private void PaginaEchipaManager_Load(object sender, EventArgs e)
+        {
+
+
+            SqlConnection conn1 = new SqlConnection();
+            SqlDataReader reader1 = Globals.executeQuery("Select Nume, Prenume, Id from Angajat where Email = '" + Globals.EmailManager + "'", out conn1);
+            string numesiprenume = "";
+            while (reader1.Read())
+            {
+                numesiprenume += reader1["Nume"];
+                numesiprenume += " ";
+                numesiprenume += reader1["Prenume"];
+                Globals.IdManager = (int)reader1["Id"];
+            }
+            reader1.Close();
+            conn1.Close();
+            label1.Text = numesiprenume;
+
+
+            SqlConnection conn = new SqlConnection();
+            SqlDataReader reader = Globals.executeQuery("Select Nume, Prenume, IdEchipa, ManagerId from Angajat where ManagerId='"+Globals.IdManager+"' and IdEchipa= '" + Globals.IdEchipa + "'", out conn);
+            while (reader.Read())
+            {
+                string nume = (string)reader["Nume"];
+                string prenume = (string)reader["Prenume"];
+                int managerId = (int)reader["ManagerId"];
+                int idEchipa = (int)reader["Idechipa"];
+
+
+                VizualizareEchipaManager angajat = new VizualizareEchipaManager(nume, prenume,idEchipa, managerId);
+                lista.Add(angajat);
+            }
+
+            dataGridView1.DataSource = lista;
+
+            conn.Close();
+
+            //poza echipei 
+            byte[] poza = { };
+            bool isOk = true;
+            string query2 = "SELECT Poza FROM Angajat WHERE IdEchipa ='" + Globals.IdEchipa + "'";
+            SqlConnection connection2;
+            SqlDataReader reader2 = Globals.executeQuery(query2, out connection2);
+
+            while (reader2.Read())
+            {
+                if (reader2["Poza"] != DBNull.Value)
+                    poza = (byte[])reader2["Poza"];
+                else
+                    isOk = false;
+
+            }
+            reader2.Close();
+            connection2.Close();
+            if (isOk == true)
+                pictureBox1.Image = System.Drawing.Image.FromStream(new MemoryStream(poza));
         }
     }
 }
