263933dab7d2e24fd62e5154dc61b1d2b852ed0d
Teodor Ungureanu <teodor.ungureanu@totalsoft.ro>
2022-09-13 12:00:38 +0300

    CreareConcediu update

--- AplicatieConcediu/Pagini_Concedii/Pagina_CreareConcediu.cs
+++ AplicatieConcediu/Pagini_Concedii/Pagina_CreareConcediu.cs
@@ -22,7 +22,7 @@ namespace AplicatieConcediu
 {
     public partial class Pagina_CreareConcediu : Form
     {
-       
+
         public Pagina_CreareConcediu()
         {
             InitializeComponent();
@@ -49,12 +49,12 @@ namespace AplicatieConcediu
             return list;
         }
         List<int> ListaId = new List<int>();
-		private readonly object inlocuitor;
+        private readonly object inlocuitor;
 
-		public List<XD.Models.Angajat> GetInlocuitori()
+        public List<XD.Models.Angajat> GetInlocuitori()
         {
             //Globals.IdUserActual1 = 2;
-            var url = "http://localhost:5107/CreareConcediu/GetInlocuitor/" + Globals.IdUserActual1 ;
+            var url = "http://localhost:5107/CreareConcediu/GetInlocuitor/" + Globals.IdUserActual1;
             var httpRequest = (HttpWebRequest)WebRequest.Create(url);
             List<XD.Models.Angajat> list = new List<XD.Models.Angajat>();
             var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
@@ -70,149 +70,146 @@ namespace AplicatieConcediu
             }
             return list;
         }
-
-		public Dictionary<int, int > GetNrZileConcediu()
-		{
-			var url = "http://localhost:5107/CreareConcediu/GetNrZileConcediu/" + Globals.IdUserActual1;
-			var httpRequest = (HttpWebRequest)WebRequest.Create(url);
-			//List<DictionarZileConcediu> list = new List<DictionarZileConcediu>();
-			Dictionary<int, int> dictPreluatDinBackend = new Dictionary<int, int>();
-			var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
-			using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-			{
-				var result = streamReader.ReadToEnd();
-				dictPreluatDinBackend = JsonConvert.DeserializeObject<Dictionary<int,int>>(result);
-			}
-			return dictPreluatDinBackend;
-		}
-
-		Dictionary<int, int> DictionarZile = new Dictionary<int, int>();
-
-		private void Pagin_CreareConcediu_Load(object sender, EventArgs e)
+        public List<XD.Models.Angajat> GetInlocuitoriIndisponibili()
         {
-            
+            //Globals.IdUserActual1 = 2
+            var url = "http://localhost:5107/CreareConcediu/GetInlocuitoriIndisponibili?dataIncepere=" + dateTimePickerDataIncepere.Value + "&dataIncetare=" + dateTimePickerDataIncetare.Value + "&AngajatId=" + Globals.IdUserActual1;
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            List<XD.Models.Angajat> list = new List<XD.Models.Angajat>();
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                var settings = new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore,
+                    MissingMemberHandling = MissingMemberHandling.Ignore
+                };
+                list = JsonConvert.DeserializeObject<List<XD.Models.Angajat>>(result, settings);
+            }
+            return list;
+        }
+        public Dictionary<int, int> GetNrZileConcediu()
+        {
+            var url = "http://localhost:5107/CreareConcediu/GetNrZileConcediu/" + Globals.IdUserActual1;
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            //List<DictionarZileConcediu> list = new List<DictionarZileConcediu>();
+            Dictionary<int, int> dictPreluatDinBackend = new Dictionary<int, int>();
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                dictPreluatDinBackend = JsonConvert.DeserializeObject<Dictionary<int, int>>(result);
+            }
+            return dictPreluatDinBackend;
+        }
+
+        Dictionary<int, int> DictionarZile = new Dictionary<int, int>();
+
+        private void Pagin_CreareConcediu_Load(object sender, EventArgs e)
+        {
+
             DictionarZile = GetNrZileConcediu();
-			btnAdaugare.Enabled = false;
+            btnAdaugare.Enabled = false;
             // TODO: This line of code loads data into the 'dataSet1.TipConcediu' table. You can move, or remove it, as needed.
             //    this.tipConcediuTableAdapter.Fill(this.dataSet1.TipConcediu);
             List<TipConcediu> lista = new List<TipConcediu>();
             List<Angajat> listaAngajat = new List<Angajat>();
 
 
-			//try
-			//{
-			//    //sql connection object
-			//    using (SqlConnection conn = new SqlConnection(Globals.ConnString))
-			//    {
+            //try
+            //{
+            //    //sql connection object
+            //    using (SqlConnection conn = new SqlConnection(Globals.ConnString))
+            //    {
 
-			//        //retrieve the SQL Server instance version
-			//        string query = string.Format(" SELECT * FROM TipConcediu");          
-			//        Globals.EmailUserActual = "popescuioan@yahoo.com";
-			//        string query2 = string.Format("SELECT * FROM Angajat WHERE idEchipa = (SELECT idEchipa FROM Angajat WHERE Email =  '"+ Globals.EmailUserActual +"') and Email <> '" + Globals.EmailUserActual + "'");
-			//        //define the SqlCommand object
-			//        SqlCommand cmd = new SqlCommand(query, conn);
-			//        SqlCommand cmd2 = new SqlCommand(query2, conn);
-			//        //open connection
-			//        conn.Open();
+            //        //retrieve the SQL Server instance version
+            //        string query = string.Format(" SELECT * FROM TipConcediu");          
+            //        Globals.EmailUserActual = "popescuioan@yahoo.com";
+            //        string query2 = string.Format("SELECT * FROM Angajat WHERE idEchipa = (SELECT idEchipa FROM Angajat WHERE Email =  '"+ Globals.EmailUserActual +"') and Email <> '" + Globals.EmailUserActual + "'");
+            //        //define the SqlCommand object
+            //        SqlCommand cmd = new SqlCommand(query, conn);
+            //        SqlCommand cmd2 = new SqlCommand(query2, conn);
+            //        //open connection
+            //        conn.Open();
 
-			//        //execute the SQLCommand
-			//        SqlDataReader dr = cmd.ExecuteReader();
+            //        //execute the SQLCommand
+            //        SqlDataReader dr = cmd.ExecuteReader();
 
 
-			//        //check if there are records
-			//        if (dr.HasRows)
-			//        {
-			//            while (dr.Read())
-			//            {
-			//                var tipconcediu = new TipConcediu();
-			//                var x = dr.GetValue(0);
-			//                var y = dr.GetValue(1);
-			//                var z = dr.GetValue(2);
-			//                tipconcediu.id = (int)x;
-			//                tipconcediu.Nume = y.ToString();
-			//                tipconcediu.Cod = z.ToString();
-			//                lista.Add(tipconcediu);
-			//            }
-			//        }
-			//        else
-			//        {
-			//            Console.WriteLine("No data found.");
-			//        }
+            //        //check if there are records
+            //        if (dr.HasRows)
+            //        {
+            //            while (dr.Read())
+            //            {
+            //                var tipconcediu = new TipConcediu();
+            //                var x = dr.GetValue(0);
+            //                var y = dr.GetValue(1);
+            //                var z = dr.GetValue(2);
+            //                tipconcediu.id = (int)x;
+            //                tipconcediu.Nume = y.ToString();
+            //                tipconcediu.Cod = z.ToString();
+            //                lista.Add(tipconcediu);
+            //            }
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("No data found.");
+            //        }
 
-			//        //close data reader
-			//        dr.Close();
+            //        //close data reader
+            //        dr.Close();
 
-			//        SqlDataReader dr2 = cmd2.ExecuteReader();
-			//        Angajat inlocuitor; 
-			//        if (dr2.HasRows)
-			//        {
-			//            while (dr2.Read())
-			//            {
-			//                inlocuitor = new Angajat();
-			//                var x = dr2.GetValue(0);
-			//                var y = dr2.GetValue(1);
-			//                var z = dr2.GetValue(2);
-			//                var n = dr2.GetValue(12);
-			//                inlocuitor.id = (int)x;
-			//                inlocuitor.Nume = y.ToString();
-			//                inlocuitor.Prenume = z.ToString();
-			//                listaAngajat.Add(inlocuitor);
-			//            }
-			//        }
-			//        else
-			//        {
-			//            Console.WriteLine("No data found.");
-			//        }
+            //        SqlDataReader dr2 = cmd2.ExecuteReader();
+            //        Angajat inlocuitor; 
+            //        if (dr2.HasRows)
+            //        {
+            //            while (dr2.Read())
+            //            {
+            //                inlocuitor = new Angajat();
+            //                var x = dr2.GetValue(0);
+            //                var y = dr2.GetValue(1);
+            //                var z = dr2.GetValue(2);
+            //                var n = dr2.GetValue(12);
+            //                inlocuitor.id = (int)x;
+            //                inlocuitor.Nume = y.ToString();
+            //                inlocuitor.Prenume = z.ToString();
+            //                listaAngajat.Add(inlocuitor);
+            //            }
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("No data found.");
+            //        }
 
-			//        //close connection
+            //        //close connection
 
-			//        dr2.Close();
-			//        conn.Close();
+            //        dr2.Close();
+            //        conn.Close();
 
-			cbTipConcediu.DisplayMember = "Nume";
-			cbTipConcediu.ValueMember = "Id";
-			cbTipConcediu.DataSource = new BindingSource(GetTipuriConcediu(), null);
+            cbTipConcediu.DisplayMember = "Nume";
+            cbTipConcediu.ValueMember = "Id";
+            cbTipConcediu.DataSource = new BindingSource(GetTipuriConcediu(), null);
             cbTipConcediu.SelectedItem = null;
-                    
 
-            List<XD.Models.Angajat> list2 = GetInlocuitori();
-            foreach(var angajat in list2)
+            List<XD.Models.Angajat> list2 = GetInlocuitoriIndisponibili();
+
+            foreach (var angajat in list2)
             {
                 Angajat inlocuitor = new Angajat();
                 inlocuitor.Nume = angajat.Nume;
                 inlocuitor.Prenume = angajat.Prenume;
                 inlocuitori.Add(inlocuitor);
                 ListaId.Add(angajat.Id);
-				
 
-			}
-			//string emailFolositLaSelect;
-			//if (Globals.EmailUserViewed != "")
-			//{
-			//	emailFolositLaSelect = Globals.EmailUserViewed;
-			//}
-			//else
-		//	{
-			//	emailFolositLaSelect = Globals.EmailUserActual;
-			//}
-			//Angajat a = new Angajat();
-			 //a = GetNrZileConcediu(emailFolositLaSelect);
 
-			//lbRezultatZileConcediuDisponibile.Text = a.NumarZileConceiduRamase.ToString();
+            }
+           
 
-			cbInlocuitori.DataSource = new BindingSource(inlocuitori, null);
-                    cbInlocuitori.DisplayMember = "NumeComplet";
-                    cbInlocuitori.ValueMember = "id";
+            cbInlocuitori.DataSource = new BindingSource(inlocuitori, null);
+            cbInlocuitori.DisplayMember = "NumeComplet";
+            cbInlocuitori.ValueMember = "id";
             cbInlocuitori.SelectedItem = null;
-
-
-
-
-            //catch (Exception ex)
-            //{
-            //    //display error message
-            //    Console.WriteLine("Exception: " + ex.Message);
-            //}
 
             cbTipConcediu.Text = "";
             cbInlocuitori.Text = "";
@@ -222,11 +219,7 @@ namespace AplicatieConcediu
 
         }
 
-		
-
-
-
-		private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
             btnAdaugare.Enabled = true;
             DateTime dataIncepere = Convert.ToDateTime(dateTimePickerDataIncepere.Value);
@@ -242,9 +235,22 @@ namespace AplicatieConcediu
             {
                 tbTotalZileConcediuCreat.Text = ZileConcediu(dataIncepere, dataIncetare).ToString();
             }
-		
+            List<XD.Models.Angajat> list2 = GetInlocuitoriIndisponibili();
 
-		}
+            foreach (var angajat in list2)
+            {
+                Angajat inlocuitor = new Angajat();
+                inlocuitor.Nume = angajat.Nume;
+                inlocuitor.Prenume = angajat.Prenume;
+                inlocuitori.Add(inlocuitor);
+                ListaId.Add(angajat.Id);
+
+            }
+
+            cbInlocuitori.DataSource = new BindingSource(inlocuitori, null);
+            cbInlocuitori.DisplayMember = "NumeComplet";
+            cbInlocuitori.ValueMember = "id";
+        }
 
         public static int ZileConcediu(DateTime firstDay, DateTime lastDay)
         {
@@ -261,9 +267,6 @@ namespace AplicatieConcediu
             holidays.Add(new DateTime(year, 12, 25)); // Prima zi de Craciun
             holidays.Add(new DateTime(year, 12, 26)); // A doua zi de Craciun
 
-
-
-
             firstDay = firstDay.Date;
             lastDay = lastDay.Date;
             TimeSpan span = lastDay - firstDay;
@@ -278,8 +281,6 @@ namespace AplicatieConcediu
                     lastDayOfWeek += 7;
                 if (firstDayOfWeek <= 6)
                 {
-
-
 
                     if (lastDayOfWeek >= 7)
                         zileConcediu -= 2; // Altfel scadem doar sambata si duminica
@@ -317,8 +318,22 @@ namespace AplicatieConcediu
             {
                 tbTotalZileConcediuCreat.Text = ZileConcediu(dataIncepere, dataIncetare).ToString();
             }
-		
-		}
+            List<XD.Models.Angajat> list2 = GetInlocuitoriIndisponibili();
+
+            foreach (var angajat in list2)
+            {
+                Angajat inlocuitor = new Angajat();
+                inlocuitor.Nume = angajat.Nume;
+                inlocuitor.Prenume = angajat.Prenume;
+                inlocuitori.Add(inlocuitor);
+                ListaId.Add(angajat.Id);
+
+            }
+
+            cbInlocuitori.DataSource = new BindingSource(inlocuitori, null);
+            cbInlocuitori.DisplayMember = "NumeComplet";
+            cbInlocuitori.ValueMember = "id";
+        }
 
         private void buttonBack_Click(object sender, EventArgs e)
         {
@@ -327,34 +342,40 @@ namespace AplicatieConcediu
 
         private async void button1_Click(object sender, EventArgs e)
         {
-           
+
             bool isOk = true;
             if (cbTipConcediu.Text == "")
             {
                 isOk = false;
                 labelEroareTipConcediu.Text = "* Tip concediu neselectat";
             }
-            if(cbInlocuitori.Text == "")
+            if (cbInlocuitori.Text == "")
             {
                 isOk = false;
                 labelEroareInlocuitor.Text = "*Inlocuitor neselectat";
             }
-            
-            if( isOk == false)
-                {
+
+            if (isOk == false)
+            {
                 btnAdaugare.Enabled = false;
                 MessageBox.Show("Completati toate campurile!");
                 return;
             }
 
-			if (Convert.ToInt32(tbTotalZileConcediuCreat.Text) > Convert.ToInt32(lbRezultatZileConcediuDisponibile.Text))
-			{
-				btnAdaugare.Enabled = false;
-				MessageBox.Show("Numarul de zile de concediu selectat depaseste numarul de zile de concediu disponibile!");
-				return;
+            if (Convert.ToInt32(tbTotalZileConcediuCreat.Text) > Convert.ToInt32(lbRezultatZileConcediuDisponibile.Text))
+            {
+                btnAdaugare.Enabled = false;
+                MessageBox.Show("Numarul de zile de concediu selectat depaseste numarul de zile de concediu disponibile!");
+                return;
 
-			}
-			string data_incepere = dateTimePickerDataIncepere.Text;
+            }
+            if(Convert.ToInt32(lbRezultatZileConcediuDisponibile.Text) <= 0)
+            {
+                btnAdaugare.Enabled = false;
+                MessageBox.Show("Zile de concediu insuficiente!");
+                return;
+            }
+            string data_incepere = dateTimePickerDataIncepere.Text;
             string data_incetare = dateTimePickerDataIncetare.Text;
             string motiv = tbMotiv.Text;
             string data_incepre_formatata = data_incepere.Substring(data_incepere.IndexOf(',') + 2, data_incepere.Length - 2 - data_incepere.IndexOf(','));
@@ -422,14 +443,14 @@ namespace AplicatieConcediu
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-			if (cbTipConcediu.SelectedValue == null)
+            if (cbTipConcediu.SelectedValue == null)
             {
                 lbRezultatZileConcediuDisponibile.Text = "";
                 return;
             }
-            
+
             lbRezultatZileConcediuDisponibile.Text = DictionarZile[(int)cbTipConcediu.SelectedValue].ToString();
 
-		}
+        }
     }
 }
